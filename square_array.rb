33756f578ced07e883930948162240a78738bbe3
Melissa Sperry <sperry.mel@gmail.com>
2020-02-15 20:29:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  numbers = [1,2,3]
+  expect(numbers).to_not receive(:collect)
+  expect(numbers).to_not receive(:map)
+  expect(numbers).to_not receive(:inject)
+  square_array(numbers)
 end
